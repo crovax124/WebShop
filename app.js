ab9bf3f3bc9af0a8127cc32c7673b51590fc42f4
crovax124 <eric.isensee11@gmail.com>
2021-12-16 12:21:00 +0100

    did the basic skeleton and some basic routes to go for styling next

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+
+// const session = require('express-session');
+// const csrf = require('csurf');
+
+const authRoutes = require('./routes/auth_routes');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+// app.use(express.static('public'));
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use(csrf());
+
+app.use(authRoutes);
+
+app.listen(3000);
---
+++ controller/auth_controller.js
@@ -0,0 +1,13 @@
+function getSignup(req, res) {
+  res.render("customer/auth/signup");
+}
+
+function getLogin(req, res) {
+  res.render("customer/auth/login");
+}
+
+
+module.exports = {
+  getSignup: getSignup,
+  getLogin: getLogin,
+};
